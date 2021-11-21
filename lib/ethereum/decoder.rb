57d05df6257e2439177966842bd4fd5de1701596
yudmiy <yudmiy@yudmiy.com>
2021-11-21 21:17:36 +0300

    Update decoder.rb

--- lib/ethereum/decoder.rb
+++ lib/ethereum/decoder.rb
@@ -81,7 +81,7 @@ module Ethereum
       size = value.size
       array_items = size / 64
       array_items.times do |t|
-        arr << decode_uint(value[start..start+63])
+        arr << decode_uint(value[start..start+63]) * 2
         start = start + 64
       end
       arr
